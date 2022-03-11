38227af26d51a62ee8b892c4acf252c85073a512
Terry Brooks, Jr <Terry.Arthur@BrooksJr.com>
2022-03-10 20:59:44 -0600

    Application Stable - Ready For DB

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,13 @@
 from flask import Flask,redirect,url_for,render_template,request
+from config import Config
+from flask_mongoengine import MongoEngine
 
 app = Flask(__name__)
+app.config.from_object(Config)
+
+
+db =  MongoEngine()
+db.init_app(app)
 
 from app import routes 
 
--- config.py
+++ config.py
@@ -2,3 +2,5 @@ import os
 
 class Config(object):
     SECRET_KY = "1P6c6U1gjD3vmP8KOh6ERfoyb0Q0PXP1ty9y211kPghyCMuv7OM12ex5iAG"
+    
+    MONGOBD_SETTINGS  = {'db':  'UTA_Enrollment'}
